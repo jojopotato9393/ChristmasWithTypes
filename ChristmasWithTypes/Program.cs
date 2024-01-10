f59b58e6d77c2ba0a724bb12b780aeb25ce329a8
[Joseph Gaughan] <[jojopotato9393@gmail.com]>
2024-01-10 16:29:30 -0600

    Chirstmas stuff

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -4,20 +4,20 @@ namespace ChristmasWithTypes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             var xmas = new Christmas();
 
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"box", "car", "egg"}; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
